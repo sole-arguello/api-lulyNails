057e4f0670ed1332c0578efeff30025af608a746
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-01-17 13:08:30 -0300

    create user en db

--- backend/src/dao/factory.js
+++ backend/src/dao/factory.js
@@ -1,6 +1,8 @@
 import { configEnv } from "../config/configEnv.js";
 import { __dirname } from "../utils.js";
 
+let userDao
+
 const environment = configEnv.environment.persistence
 
 switch(environment){
@@ -11,5 +13,7 @@ switch(environment){
 
         console.log('Production Environment');
 
+        const { UserManager } = await import (`../dao/managers/userManager.js`)
+        userDao = new UserManager();
         break;
 }
