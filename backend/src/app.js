d3ed5d2440ec3205e8dc17be6f2edda692a22fac
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-01-17 14:07:52 -0300

    add routes app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
+import { routerApp } from './routes/index.js';
 
 export const app = express();
 app.use(express.json());
 
-
+app.use(routerApp);//todas las rutas
 
 
 app.use(morgan('dev'));
