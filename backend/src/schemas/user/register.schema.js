2a65c12de4ee2e46ec2812622e1cd5cbebfc442a
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-01-27 21:18:55 -0300

    create schema register user

---
+++ backend/src/schemas/user/register.schema.js
@@ -0,0 +1,39 @@
+import joi from 'joi';
+import { JoiMsgError } from '../joi.messageError.js';
+
+export const registerSchema = joi.object({
+    first_name: joi
+        .string()
+        .required()
+        .min(3)
+        .max(30)
+        .pattern(/^[a-zA-Z\s]*$/)
+        .messages({ ...JoiMsgError.errorMsgUser, ...JoiMsgError.errorMessage }),
+
+    last_name: joi
+        .string()
+        .optional()
+        .min(3)
+        .max(30)
+        .pattern(/^[a-zA-Z\s]*$/)
+        .messages({ ...JoiMsgError.errorMsgUser, ...JoiMsgError.errorMessage }),
+
+    age: joi
+        .number()
+        .integer()
+        .min(18)
+        .optional()
+        .messages(JoiMsgError.errorMessage ),
+
+    email: joi
+        .string()
+        .required()
+        .email()
+        .messages(JoiMsgError.errorMessage),
+
+    pasword: joi
+        .string()
+        .required()
+        .pattern(/^(?=.\d)(?=.[a-z])(?=.[A-Z])(?=.[¡!$%^&*()_+|~={}:";'<>¿?,.#])[a-zA-Z0-9¡!$%^&*()_+|~={}:";'<>¿?,.#].{8,}$/)
+        .messages({ ...JoiMsgError.errorMsgPassword, ...JoiMsgError.errorMessage }),
+})
