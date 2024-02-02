9b38259461326d2c86a41c7d568af06f61a9e2df
puchiflito <aguskainer1998@outlook.com>
2024-02-02 19:55:30 -0300

    Add the views of the pages.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.scss";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav/Nav";
+import { Contact, Gallery, Home, Services, Us } from "./view/Index";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p>Hello word</p>
-    </>
-  )
+    <div>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/us" element={<Us />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
