435310847d32a3ed082ef2ddb9ec3125db4328ed
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-02-09 23:41:35 -0300

    delete configuration deploy

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,20 +1,11 @@
 import express from 'express';
 import morgan from 'morgan';
 import { routerApp } from './routes/index.js';
-import path from 'path';
-
-const __dirname = decodeURIComponent(new URL('../', import.meta.url).pathname).replace(/^\/([a-zA-Z]:)/, '$1');
 
 export const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
 
 app.use(express.json());
 
 app.use(routerApp);//todas las rutas
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'))
-})
-
 
 app.use(morgan('dev'));
