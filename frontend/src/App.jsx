9e3808a3cb971c5ba4a0da5b9a6db7429d9661e8
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-03-08 19:47:57 -0300

    probando Context

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,25 @@
 import "./App.scss";
-import { Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav/Nav";
-import { Contact, Gallery, Home, Services, Us } from "./view/Index";
+// import { Routes, Route } from "react-router-dom";
+// import Nav from "./components/Nav/Nav";
+// import { Contact, Gallery, Home, Services, Us } from "./view/Index";
+import { AppointmentProvider } from "./context/AppointmentContext";
+import Appointment from "./components/appointment/Appointment";
 
 function App() {
   return (
-    <div>
-      <Nav />
+    <>
+      {/* <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/services" element={<Services />} />
         <Route path="/us" element={<Us />} />
-      </Routes>
-    </div>
+      </Routes> */}
+      <AppointmentProvider>
+        <Appointment />
+      </AppointmentProvider>
+    </>
   );
 }
 
