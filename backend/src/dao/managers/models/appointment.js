069265a91084d998969065ddd36a9e0a990a0c71
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-03-08 20:57:06 -0300

    create model appointment file

---
+++ backend/src/dao/managers/models/appointment.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const appointmentCollection = "turnos";
+
+const appointmentSchema = new mongoose.Schema({
+
+    date:{
+        type: Date,
+        required: true
+    },
+    hour:{
+        type: String,
+        required: true
+    },
+    formData:{
+        type: Object,
+        required: true
+    }
+
+    // user: {
+    //     type: mongoose.Schema.Types.ObjectId,
+    //     ref: "users",
+    //     required: true
+    // }
+});
+
+export const turnoModel = mongoose.model(appointmentCollection, appointmentSchema)
