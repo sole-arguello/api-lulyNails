5e3e0dfbde6af18ca0e89b749cc3ace5edfc444a
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-03-08 20:59:04 -0300

    create schema appointment file

---
+++ backend/src/schemas/appointment/appointment.schema.js
@@ -0,0 +1,30 @@
+import joi from 'joi';
+import { JoiMsgError } from '../joi.messageError';
+
+
+export const appointmentSchema = joi.object({
+    date: joi
+        .date()
+        .iso()
+        .required()
+        .messages(...JoiMsgError.errorMsAppointment),
+
+    hour: joi
+        .string()
+        .required()
+        .messages(...JoiMsgError.errorMsAppointment),
+    formData: joi
+        .object({
+            name: joi
+                .string()
+                .required()
+                .messages(...JoiMsgError.errorMsAppointment),
+            email: joi
+                .string()
+                .email()
+                .required()
+                .messages(...JoiMsgError.errorMsAppointment),
+        })
+        .required(),
+        
+})
