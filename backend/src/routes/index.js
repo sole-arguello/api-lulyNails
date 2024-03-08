c8a9b0f23cf57af77435de04d9344e4111090074
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-03-08 20:58:09 -0300

    add route appointment

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { userRouter } from './user.routes.js';
+import { appointmentRouter } from './appointment.routes.js';
 
 const router = Router();
 
 router.use('/api/user', userRouter);
+router.use('/api/turno', appointmentRouter);
 
 export { router  as routerApp}
