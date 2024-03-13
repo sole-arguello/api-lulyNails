8385b523fe4cfdffa3f96fc123e590e0fa1ad75c
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2024-03-13 07:44:08 -0300

    add function for error control

--- backend/src/dao/managers/appointmentManager.js
+++ backend/src/dao/managers/appointmentManager.js
@@ -22,7 +22,7 @@ export class AppointmentManager {
             const count = await this.model.countDocuments({date})
             return count
         } catch (error) {
-            logger.error('Error al contar turnos: manager -', error)
+            logger.error('Error al contar el limite de turnos: manager -', error)
             throw new Error(error)
         }
     }
@@ -31,7 +31,7 @@ export class AppointmentManager {
             const getAppointment = await this.model.findById(id)
             return getAppointment
         } catch (error) {
-            logger.error('Error al obtener turno: manager -', error)
+            logger.error('Error al obtener turno por id: manager -', error)
             throw new Error(error)
         }
     }
@@ -41,8 +41,20 @@ export class AppointmentManager {
             return getAppointment
             
         } catch (error) {
-            logger.error('Error al obtener turno: manager -', error)
+            logger.error('Error al obtener los datos del turno: manager -', error)
             throw new Error(error)  
+        }
+    }
+
+    async getAppointmentForEmail(email) {
+        try {
+            //console.log('email manager', email)
+            const getAppointment = await this.model.findOne({formData: {email}})
+            console.log('get manager', getAppointment)
+            return getAppointment
+        } catch (error) {
+            logger.error('Error al obtener el email del turno: manager -', error)
+            throw new Error(error)
         }
     }
 }
